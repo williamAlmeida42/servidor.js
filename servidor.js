c0a4e863bae6eb7654799c7ecb7996395175837d
williamAlmeida42 <162378960+williamAlmeida42@users.noreply.github.com>
2025-05-31 15:26:26 -0300

    Update servidor.js
    atualizaçao

--- servidor.js
+++ servidor.js
@@ -1,24 +1,27 @@
 const http = require('http');
 
 const servidor = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
 
   if (req.url === '/') {
     res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.end('Bem-vindo ao meu servidor!');
   } else if (req.url === '/contato') {
     res.statusCode = 200;
-    res.end('Pagina de Contato');
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.end('Página de Contato');
   } else if (req.url === '/servicos') {
     res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.end('Nossos serviços estão em construção.');
   } else {
     res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.end('Página não encontrada.');
   }
 });
 
 const PORTA = 3000;
 servidor.listen(PORTA, () => {
-  console.log(`Servidor rodando em http://localhost:/`);
+  console.log(`Servidor rodando em http://localhost:3000/`);
 });
